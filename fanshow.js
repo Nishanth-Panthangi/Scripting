2f423e45a4ede6423e2da80ccdcc2cc8f35dbc28
Nishanth Panthangi <nishanthkwel@gmail.com>
2018-01-03 16:49:23 +0530

    Update fanshow.js

--- fanshow.js
+++ fanshow.js
@@ -6,6 +6,15 @@
 // @grant        none
 // ==/UserScript==
 
+var date = 10, interval = 30;
+var theatres = {};
+theatres['Cineplanet Multiplex: Kompally'] = 1;
+theatres['Prasads: Hyderabad'] = 2;
+theatres['Sandhya 70mm: RTC X Roads'] = 3;
+theatres['Sandhya 35mm: RTC X Roads'] = 4;
+theatres['INOX: Maheshwari Parmeshwari Mall, Kachiguda'] = 5;
+var audioLink = 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3';
+
 (function() {
     'use strict';
 
