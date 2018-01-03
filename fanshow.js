650beb0c1501d1d6724e7037303a27a66e433ae5
Nishanth Panthangi <nishanthkwel@gmail.com>
2018-01-03 14:09:36 +0530

    Create fanshow.js

---
+++ fanshow.js
@@ -0,0 +1,13 @@
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Book tickets
+// @author       Nishanth
+// @match        https://in.bookmyshow.com/buytickets/agnyaathavaasi-hyderabad/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
